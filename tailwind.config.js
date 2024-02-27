8168d493405a2abe3c8daa33fece187064acf0ff
Saquib1973 <saquib.21ug2015@iiitranchi.ac.in>
2024-02-27 22:16:51 +0530

    deployment commit

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+
+const { createThemes } = require("tw-colors");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["'Inter'", "sans-serif"],
+                gelasio: ["'Gelasio'", "serif"]
+            },
+
+            colors: {
+                'black': '#1C1C1C',
+                'white': '#FFFFFF',
+                'blackFade': '#232323',
+                'green': '#37996B',
+            },
+
+            fontSize: {
+                'sm': '12px',
+                'base': '14px',
+                'xl': '16px',
+                '2xl': '20px',
+                '3xl': '28px',
+                '4xl': '38px',
+                '5xl': '50px',
+            },
+            fontFamily: {
+                Instrument: ["Instrument Sans", "sans-serif"]
+            },
+        },
+
+    },
+    plugins: [createThemes({
+        light: {
+            'white': '#FFFFFF',
+            'black': '#242424',
+            'grey': '#F3F3F3',
+            'dark-grey': '#6B6B6B',
+            'red': '#FF4E4E',
+            'transparent': 'transparent',
+            'twitter': '#1DA1F2',
+            'purple': '#8B46FF'
+        },
+        dark: {
+            'white': '#242424',
+            'black': '#F3F3F3',
+            'grey': '#2A2A2A',
+            'dark-grey': '#E7E7E7',
+            'red': '#991F1F',
+            'transparent': 'transparent',
+            'twitter': '#0E71A8',
+            'purple': '#582C8E',
+            'red': '#FF004D',
+            'grey-50': "#242424",
+            'grey-500': "#FFFFFF"
+        }
+    }),],
+};
